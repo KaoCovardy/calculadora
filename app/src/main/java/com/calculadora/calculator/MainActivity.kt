4e3409837548951674dc6fb4e7751d874b4dbd20
KaoCovardy <h_cps@hotmail.com>
2022-05-18 22:22:56 -0300

    Adição dos valores do 0 ao 9

--- app/src/main/java/com/calculadora/calculator/MainActivity.kt
+++ app/src/main/java/com/calculadora/calculator/MainActivity.kt
@@ -11,18 +11,30 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        //Adição dos valores nos botões 0 ao 9!
+        num_zero.setOnClickListener({ AcrescentarUmaExpressao(umastr = "0", limpar_dados = true) })
+        num_um.setOnClickListener({ AcrescentarUmaExpressao(umastr = "1", limpar_dados = true) })
+        num_dois.setOnClickListener({ AcrescentarUmaExpressao(umastr = "2", limpar_dados = true) })
+        num_tres.setOnClickListener({ AcrescentarUmaExpressao(umastr = "3", limpar_dados = true) })
+        num_quatro.setOnClickListener({ AcrescentarUmaExpressao(umastr = "4", limpar_dados = true) })
+        num_cinco.setOnClickListener({ AcrescentarUmaExpressao(umastr = "5", limpar_dados = true) })
+        num_seis.setOnClickListener({ AcrescentarUmaExpressao(umastr = "6", limpar_dados = true) })
+        num_sete.setOnClickListener({ AcrescentarUmaExpressao(umastr = "7", limpar_dados = true) })
+        num_oito.setOnClickListener({ AcrescentarUmaExpressao(umastr = "8", limpar_dados = true) })
+        num_nove.setOnClickListener({ AcrescentarUmaExpressao(umastr = "9", limpar_dados = true) })
     }
-    fun AcrescentarUmaExpressao(umastring: String, limpar_dados: Boolean){
-        if(resultado.text.isNotEmpty()){
+
+    fun AcrescentarUmaExpressao(umastr: String, limpar_dados: Boolean) {
+        if (resultado.text.isNotEmpty()) {
             expressao.text = ""
         }
-        if(limpar_dados){
-            resultado.text=""
-            expressao.append(umastring)
-        }else(){
+        if (limpar_dados) {
+            resultado.text = ""
+            expressao.append(umastr)
+        } else {
             expressao.append(resultado.text)
-            expressao.append(umastring)
-            resultado.text=""
+            expressao.append(umastr)
+            resultado.text = ""
         }
     }
 }

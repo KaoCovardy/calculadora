651a6f986b1669b2e0bf80c1795d51f3d9905b35
KaoCovardy <h_cps@hotmail.com>
2022-05-18 23:25:16 -0300

    Adição da biblioteca hunter para usar as expressao matematicas basicas, add ponto, e função do botão igual

--- app/src/main/java/com/calculadora/calculator/MainActivity.kt
+++ app/src/main/java/com/calculadora/calculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.calculadora.calculator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder //importado para utilizar a biblioteca dos operadores(soma, subt)
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
-        //Adição dos valores nos botões 0 ao 9!
+        //Adição dos valores nos botões 0 ao 9 e ponto
         num_zero.setOnClickListener({ AcrescentarUmaExpressao(umastr = "0", limpar_dados = true) })
         num_um.setOnClickListener({ AcrescentarUmaExpressao(umastr = "1", limpar_dados = true) })
         num_dois.setOnClickListener({ AcrescentarUmaExpressao(umastr = "2", limpar_dados = true) })
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         num_sete.setOnClickListener({ AcrescentarUmaExpressao(umastr = "7", limpar_dados = true) })
         num_oito.setOnClickListener({ AcrescentarUmaExpressao(umastr = "8", limpar_dados = true) })
         num_nove.setOnClickListener({ AcrescentarUmaExpressao(umastr = "9", limpar_dados = true) })
+        num_ponto.setOnClickListener({AcrescentarUmaExpressao(umastr = ".", limpar_dados=true)})
 
         //Adição dos Operadores
         soma.setOnClickListener({AcrescentarUmaExpressao(umastr = "+", limpar_dados = false)})
@@ -32,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         //Faz a função limpar, limpeza da EXPRESSÃO e do RESULTADO digitado/mostrado na tela
         limpar.setOnClickListener(){
             expressao.text=""
-            resultado.text=""
+            exp_resultado.text=""
         }
 
         // Realiza a função do backspace, apagando apenas um número por vez na "casa" expressão
@@ -41,21 +44,40 @@ class MainActivity : AppCompatActivity() {
             if (umvalor.isNotBlank()){
                 expressao.text=umvalor.substring(0,umvalor.length-1)
             }
-            resultado.text=""
+            exp_resultado.text=""
+        }
+        // resultado (botão igual) utilizado implementation 'net.objecthunter:exp4j:0.4.8'
+        //para importar a biblioteca de expressionbuilder (utilização dos operadores) para fazer os calculos
+        igual.setOnClickListener(){
+            try{
+                val expressao = ExpressionBuilder(expressao.text.toString()).build()
+
+                val resultado = expressao.evaluate()
+                val longResult = resultado.toLong()
+
+                if(resultado == longResult.toDouble())
+                    exp_resultado.text= longResult.toString()
+                else
+                    exp_resultado.text = resultado.toString()
+
+
+            } catch (e: Exception){
+
+            }
         }
     }
 
     fun AcrescentarUmaExpressao(umastr: String, limpar_dados: Boolean) {
-        if (resultado.text.isNotEmpty()) {
+        if (exp_resultado.text.isNotEmpty()) {
             expressao.text = ""
         }
         if (limpar_dados) {
-            resultado.text = ""
+            exp_resultado.text = ""
             expressao.append(umastr)
         } else {
-            expressao.append(resultado.text)
+            expressao.append(exp_resultado.text)
             expressao.append(umastr)
-            resultado.text = ""
+            exp_resultado.text = ""
         }
     }
 }
